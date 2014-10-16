dbaa13f541d963d59fe5a2376e3ca590f6d04458
lzhm <13795423304@163.com>
2014-10-16 19:02:30 +0800

    1

--- ServiceStrackTest1/ConClient/Program.cs
+++ ServiceStrackTest1/ConClient/Program.cs
@@ -13,10 +13,35 @@ namespace ConClient
         {
             Console.Title = "ServiceStack Console Client";
             string url = "http://localhost/WebHost/";
+            Async(url);
+        }
+
+        static void Sync(string url)
+        {
             using (var client = new JsonServiceClient(url))
             {
-                HelloResponse response = client.Get(new Hello { Name = "World!" });
+                //HelloResponse response = client.Get(new Hello { Name = "World!" });
+                HelloResponse response = client.Get<HelloResponse>("/hello1/World!");
                 Console.Write("respnose:{0}", response.Result);
+            }
+        }
+
+        static void Async(string url)
+        {
+            using (var client = new JsonServiceClient(url))
+            {
+                var response = client.GetAsync <HelloResponse>(new Hello { Name = "World!" });
+                response.Success(r => Console.Write("respnose:{0}", r.Result));
+                
+            }
+        }
+        static void Async1(string url)
+        {
+            using (var client = new JsonServiceClient(url))
+            {
+                client.GetAsync(new Hello { Name = "World!" })
+                .Success(r =>Console.Write("respnose:{0}",r.Result))
+                .Error(ex => { throw ex; });
             }
         }
     }
