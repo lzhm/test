7250435e5683d2e7b95b14f4880ce767e33d907b
lzhm <13795423304@163.com>
2014-10-16 15:24:05 +0800

    111
    111

---
+++ ServiceStrackTest1/ServiceStrackTest1/Hello.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack;
+
+namespace ServiceStrackTest1
+{
+    //Request DTO
+    [Route("/hello1")]
+    [Route("/hello1/{Name}")]
+    public class Hello:IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    //Response DTO
+    //Follows naming convention
+    public class HelloResponse : IHasResponseStatus
+    {
+        public ResponseStatus ResponseStatus { get; set; } //Automatic exception handling
+
+        public string Result { get; set; }
+    }
+
+    public class HelloService : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = "Hello world, " + request.Name
+                                        //, ResponseStatus=new ResponseStatus("111","error")  
+                                    };
+        }
+    }
+}
